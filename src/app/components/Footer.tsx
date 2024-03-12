82bde2021302373ba18c726a91a4b767321dc320
Valentina <118310524+valeamayola@users.noreply.github.com>
2024-03-12 16:56:38 -0300

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -27,7 +27,7 @@ export default function Footer() {
         <h6 className="footer-title">Ponte en contacto</h6>
         <a className="link link-hover flex"><FontAwesomeIcon className="w-4 mr-2 text-gray-500" icon={faLocationDot} />Av. Mitre 9999, CABA, Argentina.</a>
         <a className="link link-hover flex"><FontAwesomeIcon className="w-4 mr-2 text-gray-500" icon={faPhone} />(+54) 11 888 888 888</a>
-        <a className="link link-hover flex"><FontAwesomeIcon className="w-4 mr-2 text-gray-500" icon={faEnvelope} />logistic@info.com</a>
+        <a className="link link-hover flex"><FontAwesomeIcon className="w-4 mr-2 text-gray-500" icon={faEnvelope} />galvezsrl@info.com</a>
       </nav>
     </footer>
   )
