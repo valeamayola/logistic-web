bcfc77865c941e99b210372692d111b87b97dfbe
Valentina <118310524+valeamayola@users.noreply.github.com>
2024-03-12 17:16:52 -0300

    Update ServicesSection.tsx

--- src/app/components/home/ServicesSection.tsx
+++ src/app/components/home/ServicesSection.tsx
@@ -10,19 +10,19 @@ export default function ServicesSection() {
     return (
         <section className="flex flex-col md:flex-row w-full">
             <div className="flex-1">
-                <ServiceImg href="/services#almacenamiento" backgroundImage={img1} number="01" title="Construcción de casas a medida"/>
+                <ServiceImg href="/" backgroundImage={img1} number="01" title="Construcción de casas a medida"/>
             </div>
             <div className="flex-1">
-                <ServiceImg href="/services#distribucion" backgroundImage={img2} number="02" title="Construcción de edificios"/>
+                <ServiceImg href="/" backgroundImage={img2} number="02" title="Construcción de edificios"/>
             </div>
             <div className="flex-1">
-                <ServiceImg href="/services#picking-and-packing" backgroundImage={img3} number="03" title="Construcción de proyectos comerciales"/>
+                <ServiceImg href="/" backgroundImage={img3} number="03" title="Construcción de proyectos comerciales"/>
             </div>
             <div className="flex-1">
-                <ServiceImg href="/services#gestion-de-inventario" backgroundImage={img4} number="04" title="Reformas y ampliaciones"/>
+                <ServiceImg href="/" backgroundImage={img4} number="04" title="Reformas y ampliaciones"/>
             </div>
             <div className="flex-1">
-                <ServiceImg href="/services#logistica-inversa" backgroundImage={img5} number="05" title="Asesoramiento y gestión de proyectos"/>
+                <ServiceImg href="/" backgroundImage={img5} number="05" title="Asesoramiento y gestión de proyectos"/>
             </div>
         </section>
     )
